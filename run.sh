2d807cae70245da7bb014efe5eafd287048b33be
Mike Chernev <mikechernev@users.noreply.github.com>
2016-05-03 17:12:26 +0200

    Updated whitespaces

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker run --rm -i -t -v $(pwd)/MancalaWeb:/project maven  mvn -f /project/pom.xml package
-docker run --rm -i -t -v $(pwd)/MancalaApi:/project maven  mvn -f /project/pom.xml package
+docker run --rm -i -t -v $(pwd)/MancalaWeb:/project maven mvn -f /project/pom.xml package
+docker run --rm -i -t -v $(pwd)/MancalaApi:/project maven mvn -f /project/pom.xml package
 docker-compose up -d
